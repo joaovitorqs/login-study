774e676f36c06b29808bd985f4209e341706989e
João Vitor <joao_vqs@hotmail.com>
2026-04-30 18:18:52 -0300

    First commit.

---
+++ src/main/java/com/jpadev/login_study/LoginStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jpadev.login_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpadev/login_study/LoginStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpadev.login_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
